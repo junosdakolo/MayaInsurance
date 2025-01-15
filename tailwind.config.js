2966ce6f3d6079f0661f9dfd45b5d6b4d5862311
junosdakolo <junosdakolo@gmail.com>
2025-01-16 00:15:51 +0100

    2nd commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        heroColor: 'hsla(205, 98%, 27%, 0.1)',
+        SignUp: 'hsla(205, 100%, 17%, 1)',
+        mayaColor: 'hsla(18, 100%, 49%, 1)',
+        mainBG: 'hsla(240, 33%, 97%, 1)',
+      },
+    },
   },
   plugins: [],
 }
